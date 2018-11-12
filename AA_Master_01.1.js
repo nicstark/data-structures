ca4ebcc9fd5695ea75cca03c3d714f17886430db
nicstark <hello@nicstark.com>
2018-11-12 22:59:14 +0000

    master geocode json

---
+++ AA_Master_01.1.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var async = require('async'); // npm install async
+
+const files = ["final0.json","final1.json","final2.json","final3.json","final4.json","final5.json","final6.json","final7.json","final8.json","final9.json"];
+
+function readAsync(file, callback) {
+    fs.readFile(file, 'utf8', callback);
+}
+final = [];
+async.map(files, readAsync, function(err, results) {
+    // results = ['file 1 content', 'file 2 content', ...]
+    results.forEach(function (file, i){
+        temp = JSON.parse(file);
+        temp.forEach(function(obj, h){
+            final.push(obj);
+
+        })
+    })
+});
+
+
+
+setTimeout(function(){
+        fs.writeFileSync('master_final.json', JSON.stringify(final));
+
+}, 5000)
+
+    // str = JSON.stringify(final, null, 4); // (Optional) beautiful indented output.
+    // console.log(str); // Logs output to dev tools console.
+
+
+    
+    
+    
+
--- AA_Master_02.js
+++ AA_Master_02.js
@@ -6,75 +6,63 @@ var fs = require('fs');
 //using a variable from bash to avoid putting the key on github
 var apiKey = process.env.APIKEY; 
 
-const files = ["final0.json","final1.json","final2.json","final3.json","final4.json","final5.json","final6.json","final7.json","final8.json","final9.json"];
+//reading in the json file we created last week
+var content = fs.readFileSync('master_final.json');
 
+//parsing the json file so it's readable
+var addresses = JSON.parse(content);
 
-function readAsync(file, callback) {
-    location = file;
-    fs.readFile(location, 'utf8', callback);
-};
+console.log(addresses.length)
 
-async.map(files, readAsync, function(err, results) {
-    // results = ['file 1 content', 'file 2 content', ...]
+//creating an empty array to store the final data
+var meetingsData = [];
+var offsetFix = [];
 
-    results.forEach(function (file, m){   
-        //parsing the json file so it's readable
-        var addresses = JSON.parse(file);
-        
-        console.log("addresses length: " + addresses.length)
-        
-        //creating an empty array to store the final data
-        var offsetFix = [];
-        
-        var current;
-        
-        //Here we're using async to keep from the data getting mixed up as the api spits it back to us
-        //eachSeries in the async module iterates over an array and operates on each item in the array in series
-        async.eachSeries(addresses, function(value, callback2) {
-            //creating the url for the request
-            var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
-            apiRequest += 'streetAddress=' + value.address.split(' ').join('%20');
-            apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
-            apiRequest += '&format=json&version=4.01';
-        
-            console.log(apiRequest);
-            //here is the actual request, still a little fuzzy on callback function and what determines the arguments 
-            request(apiRequest, function(err, resp, body) {
-                if (err) {throw err;}
-                else {
-                    var tamuGeo = JSON.parse(body);
-                    //filling in a bunch of variable with the output of the api response
-                    var success = (tamuGeo['FeatureMatchingResultType']);
-                    console.log(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'])
-                    var preLat =(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'])
-                    var preLong = (tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Longitude'])
-                    var prelatLong = {lat:preLat, long: preLong}
-                    var preAddress = value += ' New York, NY';
-                    //building an object to hold all the variables and the latlong sub object
-                    var obj = {address:preAddress, latLong: prelatLong};
-                    //pushing the object to the final array
-                    offsetFix.push(obj.latLong);
-                    value.latLong = obj.latLong;
-                    return current = obj.latLong;
-                }
-            });
-        
-            setTimeout(callback2, 1000);
-        
-        }), 
-        
-        function() {
-            //writing the array to a json text in proper json format
-            console.log(offsetFix);
-            console.log(offsetFix.length);
-            for (i=0; i< addresses.length; i++){
-                addresses[i].latLong = offsetFix[i];
-            };
-        };
-            
-        fs.writeFileSync(m+'_meeting_geocode.json', JSON.stringify(addresses));
-        console.log('*** *** *** *** ***');
+var current;
 
+//Here we're using async to keep from the data getting mixed up as the api spits it back to us
+//eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    //creating the url for the request
+    var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
+    apiRequest += 'streetAddress=' + value.address.split(' ').join('%20');
+    apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
+    apiRequest += '&format=json&version=4.01';
+
+    //here is the actual request, still a little fuzzy on callback function and what determines the arguments 
+    request(apiRequest, function(err, resp, body) {
+        if (err) {throw err;}
+        else {
+            var tamuGeo = JSON.parse(body);
+            //filling in a bunch of variable with the output of the api response
+            var success = (tamuGeo['FeatureMatchingResultType']);
+            console.log(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'])
+            var preLat =(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'])
+            var preLong = (tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Longitude'])
+            var prelatLong = {lat:preLat, long: preLong}
+            var preAddress = value += ' New York, NY';
+            //building an object to hold all the variables and the latlong sub object
+            var obj = {address:preAddress, latLong: prelatLong};
+            //pushing the object to the final array
+            offsetFix.push(obj.latLong);
+            value.latLong = obj.latLong;
+            return current = obj.latLong;
+        }
     });
+
+    setTimeout(callback, 1000);
+
+}, function() {
+    //writing the array to a json text in proper json format
+    console.log(offsetFix);
+    console.log(offsetFix.length);
+    for (i=0; i< addresses.length; i++){
+        addresses[i].latLong = offsetFix[i];
+    }
     
+    fs.writeFileSync('master_final_geocode.json', JSON.stringify(addresses));
+    console.log('*** *** *** *** ***');
+    console.log('Number of meetings in this zone: ');
+    console.log(meetingsData.length);
 });
+
